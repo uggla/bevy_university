2e61cfa47ca4a36aa611bb8138c41876f1464436
Uggla <uggla@free.fr>
2024-11-24 00:28:13 +0100

    feat: Add systems

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_systems(Startup, my_first_system)
+        .add_systems(
+            Update,
+            (my_second_system, my_third_system, my_fourth_system),
+            // (my_second_system, my_third_system, my_fourth_system).chain(),
+        )
+        .run();
+}
+
+fn my_first_system() {
+    println!("Hello, bevy! I'm the first system!");
+}
+
+fn my_second_system() {
+    println!("I'm the second system!");
+}
+
+fn my_third_system() {
+    println!("I'm the third system!");
+}
+
+fn my_fourth_system() {
+    println!("I'm the fourth system!");
 }
