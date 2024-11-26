97db85f4bc03487b7a1b8f361d8b1b1e22eae9c9
Bastien Sevajol <contact@bux.fr>
2024-11-26 13:33:48 +0100

    fix server.py

--- server.py
+++ server.py
@@ -7,29 +7,20 @@ import subprocess
 import sys
 
 PORT = 8000
-FILTERED_DIRS = [
-    ".cargo",
-    "reveal.js",
-    ".git",
-    "src",
-    "target",
-]
+SLIDES_DIR = "slides"
 Handler = http.server.SimpleHTTPRequestHandler
+
 
 def check_tools():
     try:
-        subprocess.check_output(
-            "which staticjinja >/dev/null 2>&1"
-            ,shell=True
-        )
+        subprocess.check_output("which staticjinja >/dev/null 2>&1", shell=True)
 
         if not os.path.exists("reveal.js/package.json"):
             raise FileNotFoundError
     except subprocess.CalledProcessError:
         print(
-            "staticjinja is missing, "
-            "Please install it with pip install staticjinja",
-            file=sys.stderr
+            "staticjinja is missing, " "Please install it with pip install staticjinja",
+            file=sys.stderr,
         )
         sys.exit(1)
     except FileNotFoundError:
@@ -37,26 +28,23 @@ def check_tools():
             "Could not find reveal.js, "
             "Please get the submodules using "
             "git submodule init && git submodule update",
-            file=sys.stderr
+            file=sys.stderr,
         )
 
 
 def serve_content():
+    os.chdir(SLIDES_DIR)
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print("serving at http://localhost:{}".format(PORT))
         httpd.serve_forever()
 
-def build_presentations():
-    directories = []
-    for dir in os.listdir("."):
-        if dir not in FILTERED_DIRS and os.path.isdir(dir):
-            directories.append(dir)
 
-    for dir in directories:
-        print("Building presentation for {}".format(dir))
-        os.chdir(dir)
-        subprocess.check_output("staticjinja build", shell=True)
-        os.chdir("..")
+def build_presentations():
+    print("Building presentation for {}".format(SLIDES_DIR))
+    os.chdir(SLIDES_DIR)
+    subprocess.check_output("staticjinja build", shell=True)
+    os.chdir("..")
+
 
 def main():
     check_tools()
