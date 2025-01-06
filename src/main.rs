7e0b6f0461cbceefe775e2cc24c8ec9d345ca0f4
Uggla <uggla@free.fr>
2025-01-06 22:21:06 +0100

    feat: Add some fixes for wasm

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod states;
 mod vessel;
 
 use asteroids::AsteroidsPlugin;
-use bevy::{prelude::*, window::WindowResolution};
+use bevy::{asset::AssetMetaCheck, prelude::*, window::WindowResolution};
 use bevy_rapier2d::plugin::{NoUserData, RapierPhysicsPlugin};
 #[cfg(debug_assertions)]
 use bevy_rapier2d::render::RapierDebugRenderPlugin;
@@ -22,15 +22,25 @@ struct CurrentLevel(u8);
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "Bevy University".to_string(),
-                    resizable: false,
-                    resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Bevy University".to_string(),
+                        resizable: false,
+                        resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                // prevents blurry sprites
+                .set(ImagePlugin::default_nearest())
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics in web builds on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
                     ..default()
                 }),
-                ..default()
-            }),
             StatesPlugin,
             CameraPlugin,
             RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(VESSEL_WIDTH * 0.5 / 5.0),
